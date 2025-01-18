559ccbfed6fd7a0365b3d1353d41cc8e413b8b2a
egiputra <egiputra450@gmail.com>
2025-01-18 12:11:19 +0700

    create app content

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import { useState } from 'react'
 
-function App() {
-  // const [count, setCount] = useState(0)
+let currentTasks = [
+  { name: 'Tugas 1', dueDate: Date() }
+]
 
+let completedTasks = [
+  { name: 'Tugas 2', dueDate: Date() }
+]
+
+function App() {
+  const [tasks, setTask] = useState({ 
+    current: currentTasks, 
+    completed: completedTasks 
+  })
+
+  // TODO: Both current and completed task should be sorted by date
+  // TODO: Each list should be can edit
+  // TODO: Completed task should be display as button by default
   return (
     <div className="container">
       <h1>Simple todo list</h1>
@@ -11,14 +25,27 @@ function App() {
         <input type="date"/>
         <button type="submit">Add task</button>
       </form>
+      <h2>Current tasks</h2>
       <ul>
-        <li>
-          <p>Tugas 1</p>
-          <p> Due date: <span>16 Oktober 2024, 13:00 WIB</span></p>
-        </li>
+        {tasks.current.map((task, index) => (
+          <li key={index}>
+            <p>{task.name}</p>
+            <p> Due date: <span>{task.dueDate}</span></p>
+          </li>
+        ))}
+      </ul>
+      <h2>Completed tasks</h2>
+      <ul>
+        {tasks.completed.map((task, index) => (
+          <li key={index}>
+            <p>{task.name}</p>
+            <p> Due date: <span>{task.dueDate}</span></p>
+          </li>
+        ))}
       </ul>
     </div>
   )
 }
 
+//            <p> Due date: <span>16 Oktober 2024, 13:00 WIB</span></p>
 export default App
